a931c373cae07febaccaf997a7f0f080a7f77b51
Felipeoliveeira <Felipeoliveira.aju79@gmail.com>
2024-03-20 21:51:48 -0300

    `correction`

--- aula4/index.js
+++ aula4/index.js
@@ -3,17 +3,19 @@ let horaSistema = new Date().getHours();
 boxHorario.textContent = `Agora são ${horaSistema} horas`;
 let corDeFundo = document.querySelector("body");
 let divImage = document.querySelector("div");
-let entardercerURL = "https://images.pexels.com/photos/724507/pexels-photo-724507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-let anoitecerURL = "https://images.pexels.com/photos/1539225/pexels-photo-1539225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+let entardercerURL =
+  "https://images.pexels.com/photos/724507/pexels-photo-724507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+let anoitecerURL =
+  "https://images.pexels.com/photos/1539225/pexels-photo-1539225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-if (horaSistema > 12 && horaSistema < 18) {
-  boxHorario.textContent = `Agora são ${horaSistema} horas da tarde`;
-  corDeFundo.style.backgroundColor = "rgb(255, 127, 80)";
-  divImage.style.backgroundImage = `url(${entardercerURL})`;
-} else if (horaSistema > 18 && horaSistema < 24) {
-  boxHorario.textContent = `Agora são ${horaSistema} horas da noite`;
-  corDeFundo.style.backgroundColor = "rgb(128, 128, 128)";
-  divImage.style.backgroundImage = `url(${anoitecerURL})`;
-} else {
+if (horaSistema <= 12) {
   boxHorario.textContent = `Agora são ${horaSistema} horas da manhã`;
+} else if (horaSistema >= 13 && horaSistema <= 18) {
+  boxHorario.textContent = `Agora são ${horaSistema} horas da tarde`;
+  corDeFundo.style.backgroundColor = "rgb(221, 123, 106)";
+  divImage.style.backgroundImage = `url(${entardercerURL})`;
+} else {
+  boxHorario.textContent = `Agora são ${horaSistema} horas da noite`;
+  corDeFundo.style.backgroundColor = "rgb(68, 67, 67)";
+  divImage.style.backgroundImage = `url(${anoitecerURL})`;
 }
