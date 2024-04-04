aebaab06d00cbf6b7179120b3f5f1e5baf846a69
Felipeoliveeira <Felipeoliveira.aju79@gmail.com>
2024-04-03 23:58:02 -0300

    `image.style.width`

--- aula6/index.js
+++ aula6/index.js
@@ -1,12 +1,28 @@
-let anoNascimento = document.getElementById('ano');
-let sexMen = document.getElementById('sexM');
-let sexFem = document.getElementById('sexF');
-let image = document.getElementById('image')
-function verificar(){
+let anoNascimento = document.getElementById("ano");
+let sexMen = document.getElementById("sexM");
+let sexFem = document.getElementById("sexF");
+let image = document.getElementById("image");
+let section = document.getElementById("section");
 
-    if( sexMen.checked && anoNascimento.value < 13 ){
-        image.src = "style/criancaHomem.png"
-        console.log('ok')
-    }
-
+function verificar() {
+  if (sexMen.checked && anoNascimento.value <= 15) {
+    image.src = "style/garotojovem.jpg";
+    image.style.width = "150PX";
+    image.style.height = "200px";
+    image.style.borderRadius = "50%";
+    section.style.height = "310px";
+    console.log("ok");
+  } else if (sexFem.checked && anoNascimento.value <= 15) {
+    image.src = "style/garotajovem.jpg";
+    image.style.width = "150PX";
+    image.style.height = "200px";
+    image.style.borderRadius = "50%";
+    section.style.height = "310px";
+  } else if (sexMen.checked && anoNascimento.value >= 16) {
+    image.src = "style/garotoadolescente.jpg";
+    image.style.width = "250PX";
+    image.style.height = "200px";
+    image.style.borderRadius = "50%";
+    section.style.height = "310px";
+  }
 }
