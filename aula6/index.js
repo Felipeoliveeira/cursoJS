3d396e5b7e7d1d1259ebe5cccc52312438b7d3fc
Felipeoliveeira <Felipeoliveira.aju79@gmail.com>
2024-04-04 22:08:07 -0300

    `object-fit: contain;`

--- aula6/index.js
+++ aula6/index.js
@@ -1,28 +1,18 @@
-let anoNascimento = document.getElementById("ano");
+let anoAtual = new Date().getFullYear();
+let anoNascimento = document.getElementById('ano');
 let sexMen = document.getElementById("sexM");
 let sexFem = document.getElementById("sexF");
 let image = document.getElementById("image");
-let section = document.getElementById("section");
 
 function verificar() {
   if (sexMen.checked && anoNascimento.value <= 15) {
     image.src = "style/garotojovem.jpg";
-    image.style.width = "150PX";
-    image.style.height = "200px";
-    image.style.borderRadius = "50%";
-    section.style.height = "310px";
-    console.log("ok");
   } else if (sexFem.checked && anoNascimento.value <= 15) {
     image.src = "style/garotajovem.jpg";
-    image.style.width = "150PX";
-    image.style.height = "200px";
-    image.style.borderRadius = "50%";
-    section.style.height = "310px";
   } else if (sexMen.checked && anoNascimento.value >= 16) {
     image.src = "style/garotoadolescente.jpg";
-    image.style.width = "250PX";
-    image.style.height = "200px";
-    image.style.borderRadius = "50%";
-    section.style.height = "310px";
+  } else if (sexFem.checked && anoNascimento.value >= 16) {
+    image.src = "style/garotaadolescente.jpg";
   }
+  console.log(aN);
 }
