dfaa8a6a6018270f972810d4f49a9d7ba9aee560
Felipeoliveeira <Felipeoliveira.aju79@gmail.com>
2024-04-06 17:18:57 -0300

    erro correction

--- aula6/index.js
+++ aula6/index.js
@@ -1,18 +1,25 @@
-let anoAtual = new Date().getFullYear();
-let anoNascimento = document.getElementById('ano');
 let sexMen = document.getElementById("sexM");
 let sexFem = document.getElementById("sexF");
 let image = document.getElementById("image");
+let mensage = document.getElementById('mensage');
 
 function verificar() {
-  if (sexMen.checked && anoNascimento.value <= 15) {
+  let anoAtual = new Date().getFullYear();
+  let anoNascimento = document.getElementById('ano').value;
+  let idade = anoAtual - Number.parseInt(anoNascimento);
+
+  if (sexMen.checked &&  idade <= 15) {
+    mensage.innerText = `Você é um jovem de ${idade} anos de idade`
     image.src = "style/garotojovem.jpg";
-  } else if (sexFem.checked && anoNascimento.value <= 15) {
+  } else if (sexFem.checked && idade <= 15) {
+    mensage.innerText = `Você é uma jovem de ${idade} anos de idade`
     image.src = "style/garotajovem.jpg";
-  } else if (sexMen.checked && anoNascimento.value >= 16) {
+  } else if (sexMen.checked && idade >= 16) {
+    mensage.innerText = `Você é um rapaz de ${idade} anos de idade`
     image.src = "style/garotoadolescente.jpg";
-  } else if (sexFem.checked && anoNascimento.value >= 16) {
+  } else if (sexFem.checked && idade >= 16) {
+    mensage.innerText = `Você é um jovem de ${idade} anos de idade`;
     image.src = "style/garotaadolescente.jpg";
   }
-  console.log(aN);
+  console.log(idade);
 }
